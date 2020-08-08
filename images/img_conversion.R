057f77d4b98292bc04bd5f37b38163b0eab134d0
nbaetge <nicholasbaetge@gmail.com>
2020-08-07 21:46:30 -0700

    update images

--- images/img_conversion.R
+++ images/img_conversion.R
@@ -1,4 +1,4 @@
 
-fig <- image_read("~/GITHUB/nbaetge.github.io/images/doc_samples.tiff")
+fig <- image_read("~/GITHUB/nbaetge.github.io/images/aciddlogo.tiff")
 fig.jpg <- image_convert(fig, "jpeg")
-image_write(fig.jpg, path = "~/GITHUB/nbaetge.github.io/images/doc_samples.jpg", format = "jpeg")
+image_write(fig.jpg, path = "~/GITHUB/nbaetge.github.io/images/aciddlogo.jpg", format = "jpeg")
