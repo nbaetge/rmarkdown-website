f1cd402d3a4ffcc69fc56fe4310b2f5275c72cb6
nbaetge <nicholasbaetge@gmail.com>
2020-08-10 17:53:16 -0700

    major push!!

--- images/img_conversion.R
+++ images/img_conversion.R
@@ -1,4 +1,4 @@
 
-fig <- image_read("~/GITHUB/nbaetge.github.io/images/aciddlogo.tiff")
+fig <- image_read("~/GITHUB/nbaetge.github.io/images/BiosScope-emblem.png")
 fig.jpg <- image_convert(fig, "jpeg")
-image_write(fig.jpg, path = "~/GITHUB/nbaetge.github.io/images/aciddlogo.jpg", format = "jpeg")
+image_write(fig.jpg, path = "~/GITHUB/nbaetge.github.io/images/BiosScope-emblem.jpeg", format = "jpeg")
