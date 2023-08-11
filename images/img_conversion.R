611dfc1031c36e8de588e2592aa41c286af9634c
nbaetge <nicholasbaetge@gmail.com>
2023-08-11 14:10:34 -0700

    major content update

--- images/img_conversion.R
+++
@@ -1,4 +0,0 @@
-
-fig <- image_read("~/GITHUB/nbaetge.github.io/images/BiosScope-emblem.png")
-fig.jpg <- image_convert(fig, "jpeg")
-image_write(fig.jpg, path = "~/GITHUB/nbaetge.github.io/images/BiosScope-emblem.jpeg", format = "jpeg")
